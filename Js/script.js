44887e98737fac095f11e475e5191b3ff00b1747
Tapadip16 <tapadippaul@gmail.com>
2023-02-19 15:02:37 +0530

    My Portfolio website

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+/* ======== Typing Animation ======== */
+var typed = new Typed(".typing",{
+    strings:["","Web Developer", "Web Designer", "UI/UX Designer"],
+    typeSpeed:90,
+    BackSpeed:60,
+    loop:true
+})
---
+++ Js/style-switcher.js
@@ -0,0 +1,31 @@
+/* ======== Toggle Style Switcher ======== */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+/* ======== Hide Style - Switcher On Scroll ======== */
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+
+/* ======== Theme Light & Dark Mode ======== */
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
